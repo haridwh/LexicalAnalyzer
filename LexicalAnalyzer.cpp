b8c2495c0547321830a2777d70610ce76d2b99b3
skday <haridwiharyono@gmail.com>
2016-10-26 15:47:41 +0700

    Add Trim

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -2,97 +2,145 @@
 #include <vector>
 using namespace std;
 
-//deklarasi fungsi, prosedur
+/**
+  *Digunakan untuk melakukan trim, sehingga tidak ada space yang berlebih
+  *misal input "    if     p then     q   ", maka setelah ditrim hasilnya akan menjadi "if p then q"
+  *
+  *@param string input, inputan dari user
+  *@return string result, inputan user yang sudah ditrim
+  */
+string trim(string input);
+
+/**
+  *Digunakan untuk memparser inputan, dan menyimpannya ke dalam vector lexic
+  *misal input "if p then", setelah diparser maka isi vector menjadi {"if","p","then"}
+  *
+  *@param string input, hasil inputan user yang telah ditrim 
+  *@param vector<string> lexic, vector untuk menyimpan hasil parser
+  */
 void parser(string input, vector<string> &lexic);
+
+/**
+  *Digunakan untuk mendapatkan token dari elemen-elemen vector lexic yang didapat dari hasil parser
+  *
+  *@param string lexic, elemen ke-i dari vector lexic
+  *@return int, token dari lexic
+  */
 int getToken(string lexic);
 
+
+/**Main Program*/
 int main(){
   string input;
-  //vector sama dengan List, jadi vector<string> sama dengan List yang isinya string
   vector<string> lexic;
   int token;
   cout<<"Input : ";
-  //getLine mirip sama cin tapi bisa nerima input pake spasi, misal "if p then", kalau pake cin cuman ke input 'if'nya aja
   getline(cin, input);
-  parser(input,lexic);//manggil prosedur parser
+  input = trim(input);
+  parser(input,lexic);
   cout<<"Output : ";
-  //ngelakuin pengulangan sebanyak isi dari vector lexic
   for (int i=0; i<lexic.size();i++){
-    token = getToken(lexic[i]);//dapetin token dari lexic ke-i, pake fungsi getToken
-    //kalau token == 0 berarti error, kalau nggak sama dengan 0 berarti bener
+    token = getToken(lexic[i]);
     if (token!=0) {
       cout<<token<<" ";
     }else{
       cout<<"error";
-      i=lexic.size();//i diset panjang isi vector lexic, jadi pengulangan langsung berhenti
+      i=lexic.size();
     }
   }
   cout<<endl;
+  return 0;
 }
 
 
-/*dipake buat ngeparser inputan, dan nyimpen di vector lexic
-misal input "if p then", setelah di parser isi vector jadi {"if","p","then"}
-@params inputan, lexic
-*/
+
+/** Fungsi dan Prosedur */
+string trim(string input){
+  if (input!=""){
+    int inpBegin = input.find_first_not_of(" ");
+    if (inpBegin == string::npos){
+       return "";
+    }
+    int inpLength = input.find_last_not_of(" ") - inpBegin + 1;    
+    string result = input.substr(inpBegin,inpLength);
+  
+    int spaceBegin;
+    int spaceLength;
+    int begin=0;
+    spaceBegin = result.find_first_of(" ",begin);
+    while(spaceBegin != string::npos){  
+      spaceLength = result.find_first_not_of(" ",spaceBegin) - spaceBegin;
+      result.replace(spaceBegin, spaceLength, " ");
+      begin=spaceBegin+1;
+      spaceBegin = result.find_first_of(" ",begin);
+    }
+    return result;
+  }else{
+    return "";
+  }
+}
+
 void parser(string input, vector<string> &lexic){
-  string kata=""; //variabel buat naroh hasil perkata
-  //ngelakuin pengulangan sebanyak length input
+  string kata="";
   for (int i=0; i<input.length(); i++) {
-    if (input[i]==' ') {//ngecek input posisi ke-i == spasi atau nggak, misal not q, berarti not harus diinsert dulu sebelum lanjut ke q
-      lexic.push_back(kata); //ngeinsert last kata ke dalem vector lexic
-      kata=""; //kata diset kosong lagi
-    }else if (input[i]=='(') {//ngecek input posisi ke-i == '('
-      if (kata != "") { //ngecek variabel kata kosong atau nggak, misal inputan if(p and q), berarti sebelum '(' ada kata "if" jadi harus diinsert dulu
-        lexic.push_back(kata);//insert last kata
+    if (input[i]==' ') {
+      if (input[i-1] !='(' && input[i-1]!=')')
+      {
+        lexic.push_back(kata);
         kata="";
       }
-      lexic.push_back(string(1,input[i]));//insert last '(', pake string(1,input[i]) karena input[i] char jadi harus diubah dulu ke string
-    }else if (input[i]==')') {//ngecek input posisi ke-i == ')'
-      if (kata != "") { //ngecek variabel kata kosong atau nggak, misal inputan if(p and q), berarti sebelum ')' ada kata "q" jadi harus diinsert dulu
+    }else if (input[i]=='(') {
+      if (kata != "") {
+        lexic.push_back(kata);
+        kata="";
+      }
+      lexic.push_back(string(1,input[i]));
+    }else if (input[i]==')') {
+      if (kata != "") {
         lexic.push_back(kata);
         kata="";
       }
       lexic.push_back(string(1,input[i]));
     }else{
-      kata+=input[i]; //kata = kata+input[i], misal variabel kata isinya "no" terus input[i] isinya "t" jadi hasilnya "not"
+      kata+=input[i];
     }
   }
-  if (kata!="") { //ngecek setelah pengulangan variabel kata masih ada isinya atau nggak, misal if a then, berarti setelah keluar pengulangan variabel kata ada isi "then" jadi harus diinsert
+  if (kata!="") {
     lexic.push_back(kata);
   }
 }
 
-/*fungsi buat ngedapetin token dari lexic yang didapet dari inputan
-@params lexic*/
 int getToken(string lexic){
-  if (lexic[0]=='p' || lexic[0]=='q' || lexic[0]=='r' || lexic[0]=='s') {//ngecek == proposisi, kalau iya return 1
-    return 1;
-  }else if(lexic[0]=='n' && lexic[1]=='o' && lexic[2]=='t'){ //ngecek == not
-    return 2;
-  }else if(lexic[0]=='a' && lexic[1]=='n' && lexic[2]=='d'){  //ngecek == and
-    return 3;
-  }else if(lexic[0]=='o' && lexic[1]=='r'){  //ngecek == or
-    return 4;
-  }else if (lexic[0]=='x' && lexic[1]=='o' && lexic[2]=='r') {  //ngecek == xor
-    return 5;
-  }else if (lexic[0]=='i' && lexic[1]=='f') {  //ngecek == if
-    if (lexic.length()==3) { //ngecek panjang lexic kalau panajangnya 3 ada kemungkinan lexic isinya "iff"
-      if (lexic[2] =='f') { //ngecek char lexic ke-3 sama dengan f gak, kalau iya berarti lexic == iff
+  switch (lexic.length()){
+    case 1:
+      if (lexic[0]=='p' || lexic[0]=='q' || lexic[0]=='r' || lexic[0]=='s') {
+        return 1;
+      }else if (lexic[0]=='(') {
+        return 9;
+      }else if (lexic[0]==')') {
+        return 10;
+      }
+    case 2:
+      if (lexic[0]=='o' && lexic[1]=='r') {
+        return 4;
+      }else if (lexic[0]=='i' && lexic[1]=='f') {  
+        return 6;
+      }
+    case 3:
+      if(lexic[0]=='n' && lexic[1]=='o' && lexic[2]=='t'){
+        return 2;
+      }else if(lexic[0]=='a' && lexic[1]=='n' && lexic[2]=='d'){ 
+        return 3;
+      }else if (lexic[0]=='x' && lexic[1]=='o' && lexic[2]=='r') {  
+        return 5;
+      }else if (lexic[0]=='i' && lexic[1]=='f' && lexic[2]=='f'){
         return 8;
       }
-    }else if (lexic.length()==2) {  //ngecek panjang sama dengan 2, kalau iya berati if
-      return 6;s
-    }else{ //kalau panjang lebih dari 3, misal "ifff" berarti error
+    case 4:
+      if (lexic[0]=='t' && lexic[1]=='h' && lexic[2]=='e' && lexic[3]=='n') {
+        return 7;
+      }
+    default:
       return 0;
-    }
-  }else if (lexic[0]=='t' && lexic[1]=='h' && lexic[2]=='e' && lexic[3]=='n') { //ngecek == then
-    return 7;
-  }else if (lexic[0]=='(') { //ngecek == (
-    return 9;
-  }else if (lexic[0]==')') { //ngecek == )
-    return 10;
-  }else{ //selain diatas berarti error
-    return 0;
   }
 }
